4f6fe5ac777557aba204a42270e223d9361cf3b0
Romeiro <theromeirofernandes@gmail.com>
2025-04-24 19:51:02 +0530

    pushing

--- Backend/server.js
+++ Backend/server.js
@@ -9,8 +9,9 @@ connectDB();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
