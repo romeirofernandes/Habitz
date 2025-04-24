201464a5317785aaf2ef1aba0e05e9519ff0ba26
Romeiro <theromeirofernandes@gmail.com>
2025-04-24 19:46:06 +0530

    MongoDB connected

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+connectDB();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
